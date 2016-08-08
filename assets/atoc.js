65b3720c8d78d7a7184786078ce687c45232b123
Willin Wang <willin@willin.org>
2016-08-08 10:06:37 +0800

    fix bug

--- assets/atoc.js
+++ assets/atoc.js
@@ -1,7 +1,7 @@
 require(["gitbook","jQuery"],function(gitbook,$){
   function resetTOC(){
     var targetUl = $('.page-inner ul:eq(0)');
-    if(targetUl && targetUl.children('li').children('a').attr('href').indexOf('#') === 0 && $('.page-inner').html().indexOf('<!-- toc -->') !== -1) {
+    if(targetUl &&  targetUl.children('li').children('a').length > 0 && targetUl.children('li').children('a').attr('href').indexOf('#') === 0 && $('.page-inner').html().indexOf('<!-- toc -->') !== -1) {
       $('.'+className+' .toggle').unbind('click');
       targetUl.before('<div class="' + className + '">' + targetUl.html() + '<div class="toggle"><i class="fa fa-align-justify"></i></div></div>');
       $('.'+className+' .toggle').bind('click',function(){
